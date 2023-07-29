6af8e94d06557e4f4d8495bb4db8f5871444824b
DeyoMateus <deyomatheus@gmail.com>
2023-07-29 18:25:23 -0300

    Fim

--- pag1.py
+++ pag1.py
@@ -3,17 +3,24 @@
 #websocket html
 #https://cdnjs.com/ (socket.io, jquery)
 from flask import Flask, render_template
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins = "*")
+
+#Funcionalidade de enviar mensagem
+@socketio.on("message")
+def gerenciar_mensagem(mensagem):
+    send(mensagem, broadcast = True)
 
 # Primeira pag = 1ª rota
 @app.route("/")
 def homepage():
-    return render_template("homepage.html")
+    return render_template("visual.html")
     
 
 # Roda o app
-app.run()
+socketio.run(app, host = "192.168.2.115") 
 
 
 
