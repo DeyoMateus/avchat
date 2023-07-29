a6bbd4bbe707a573a9b1ffd7599835d6c8031131
DeyoMateus <deyomatheus@gmail.com>
2023-07-29 15:50:54 -0300

    adicionando html

--- pag1.py
+++ pag1.py
@@ -1,10 +1,13 @@
 # Importando recursos
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 # Primeira pag = 1ª rota
-
+@app.route("/")
+def homepage():
+    return render_template("homepage.html")
+    
 
 # Roda o app
 app.run()
